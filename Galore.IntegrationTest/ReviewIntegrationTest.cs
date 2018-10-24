de52a50b37391a344260402f2ac7d150ab911c8c
disaerna <disa.erna@gmail.com>
2018-10-24 12:43:23 +0000

    review test

--- Galore.IntegrationTest/ReviewIntegrationTest.cs
+++ Galore.IntegrationTest/ReviewIntegrationTest.cs
@@ -85,7 +85,7 @@ namespace Galore.IntegrationTest
                 string updatedJson = await updatedData.Content.ReadAsStringAsync();
                 ReviewDTO updatedReview = JsonConvert.DeserializeObject<ReviewDTO>(updatedJson);
                 Assert.AreNotEqual(review.Score, updatedReview.Score);
-                Assert.AreEqual(HttpStatusCode.NoContent, updatedData.StatusCode);
+                Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
 
             }
         }
@@ -103,12 +103,31 @@ namespace Galore.IntegrationTest
 
 
         [TestMethod]
+        public async Task CreateUserReviewForTape2()
+        {
+            using(var client = new TestClientProvider().Client)
+            {
+                var response = await client.PostAsync("/v1/api/users/100/reviews/10", new StringContent(
+                    JsonConvert.SerializeObject(new ReviewInputModel()
+                    {
+                        Score = 4,
+                    }),
+                    Encoding.UTF8, "application/json"
+                ));
+                response.EnsureSuccessStatusCode();
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                Assert.IsNotNull(response.Headers.Location);
+            }
+        }
+
+        [TestMethod]
         public async Task GetAllReviewsForAllTapes()
         {
             using(var client = new TestClientProvider().Client)
             {
                 var response = await client.GetAsync("/v1/api/tapes/reviews");
                 response.EnsureSuccessStatusCode();
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
             }
         }
 
@@ -117,8 +136,42 @@ namespace Galore.IntegrationTest
         {
             using(var client = new TestClientProvider().Client)
             {
-                var response = await client.GetAsync("/v1/api/users/100/reviews");
+                var response = await client.GetAsync("/v1/api/tapes/10/reviews/100");
                 response.EnsureSuccessStatusCode();
+                string data = await response.Content.ReadAsStringAsync();
+                ReviewDTO review = JsonConvert.DeserializeObject<ReviewDTO>(data);
+                Assert.IsNotNull(review);
+                Assert.AreEqual(review.UserId, 100);
+                Assert.AreEqual(review.TapeId, 10);
+                Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            }
+        }
+
+        [TestMethod]
+        public async Task UpdateUserReviewForTape2()
+        {
+            using(var client = new TestClientProvider().Client)
+            {
+                var data = await client.GetAsync("/v1/api/tapes/10/reviews/100");
+                data.EnsureSuccessStatusCode();
+                string json = await data.Content.ReadAsStringAsync();
+                ReviewDTO review = JsonConvert.DeserializeObject<ReviewDTO>(json);
+
+                var response = await client.PutAsync("/v1/api/tapes/10/reviews/100", new StringContent(
+                    JsonConvert.SerializeObject(new ReviewInputModel()
+                    {
+                        Score = 3
+                    }),
+                    Encoding.UTF8, "application/json"
+                ));
+
+                var updatedData = await client.GetAsync("/v1/api/tapes/10/reviews/100");
+                updatedData.EnsureSuccessStatusCode();
+                string updatedJson = await updatedData.Content.ReadAsStringAsync();
+                ReviewDTO updatedReview = JsonConvert.DeserializeObject<ReviewDTO>(updatedJson);
+                Assert.AreNotEqual(review.Score, updatedReview.Score);
+                Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode);
+
             }
         }
 
@@ -127,18 +180,10 @@ namespace Galore.IntegrationTest
         {
             using(var client = new TestClientProvider().Client)
             {
-                var response = await client.GetAsync("/v1/api/users/100/reviews");
+                var response = await client.DeleteAsync("/v1/api/tapes/10/reviews/100");
                 response.EnsureSuccessStatusCode();
             }
         }
 
-        [TestMethod] async Task UpdateUserReviewForTape2()
-        {
-            using(var client = new TestClientProvider().Client)
-            {
-                var response = await client.GetAsync("/v1/api/users/100/reviews");
-                response.EnsureSuccessStatusCode();
-            }
-        }
     }
 }
