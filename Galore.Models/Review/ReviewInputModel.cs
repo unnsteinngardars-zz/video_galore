37a663a95aa5b80de285bf0be9a61ae63ab78473
unnsteinngardars <unnsteinng@gmail.com>
2018-10-24 14:12:08 +0000

    fixed input model for review to accept min 1 and max 10

--- Galore.Models/Review/ReviewInputModel.cs
+++ Galore.Models/Review/ReviewInputModel.cs
@@ -6,6 +6,7 @@ namespace Galore.Models.Review
     public class ReviewInputModel : HyperMediaModel
     {
         [Required]
+        [Range(1,10)]
         public int Score { get; set; }
     }
 }
